ee6c01d51bbdaeba82e245af65b483ce27a5717f
Felipe Lemos <fal@cesar.school>
2026-03-19 09:42:08 -0300

    Refactor exec method to execute with opcode constants

--- cpu.py
+++ cpu.py
@@ -14,39 +14,55 @@ class MiniCPU:
         self.pc += 3
         return op, a, b
 
-    def exec(self, op, a, b):
-        if op == 1:
+    def execute(self, op, a, b):
+        if op == 0x01:
             self.reg[a] = self.mem[b]
-        elif op == 2:
+        elif op == 0x02:
             self.mem[b] = self.reg[a]
-        elif op == 3:
+        elif op == 0x03:
             self.reg[a] = (self.reg[a] + self.reg[b]) & 0xFF
-        elif op == 4:
+        elif op == 0x04:
             self.reg[a] = (self.reg[a] - self.reg[b]) & 0xFF
-        elif op == 5:
+        elif op == 0x05:
             self.reg[a] = b
-        elif op == 6:
+        elif op == 0x06:
             self.zf = 1 if self.reg[a] == self.reg[b] else 0
-        elif op == 7:
+        elif op == 0x07:
             self.pc = a
-        elif op == 8:
+        elif op == 0x08:
             if self.zf:
                 self.pc = a
-        elif op == 9:
+        elif op == 0x09:
             if not self.zf:
                 self.pc = a
-        elif op == 10:
+        elif op == 0x0A:
             self.running = False
 
     def trace(self, op, a, b):
-        nomes = ["", "LOAD", "STORE", "ADD", "SUB", "MOV", "CMP", "JMP", "JZ", "JNZ", "HALT"]
-        print(f"Ciclo {self.ciclo}: {nomes[op]} {a},{b} | R0={self.reg[0]} R1={self.reg[1]} R2={self.reg[2]} R3={self.reg[3]} | PC={self.pc} ZF={self.zf}")
+        nomes = {
+            0x01: "LOAD",
+            0x02: "STORE",
+            0x03: "ADD",
+            0x04: "SUB",
+            0x05: "MOV",
+            0x06: "CMP",
+            0x07: "JMP",
+            0x08: "JZ",
+            0x09: "JNZ",
+            0x0A: "HALT"
+        }
+        nome = nomes.get(op, "???")
+        print(
+            f"Ciclo {self.ciclo}: {nome} {a},{b} | "
+            f"R0={self.reg[0]} R1={self.reg[1]} R2={self.reg[2]} R3={self.reg[3]} | "
+            f"PC={self.pc} ZF={self.zf}"
+        )
 
     def run(self):
         while self.running and self.pc < 256:
             self.ciclo += 1
             op, a, b = self.fetch()
-            self.exec(op, a, b)
+            self.execute(op, a, b)
             self.trace(op, a, b)
 
 
@@ -63,63 +79,65 @@ cpu.mem[0x15] = 40
 cpu.mem[0x16] = 8
 cpu.mem[0x17] = 22
 
-cpu.mem[0x00] = 7
+cpu.mem[0x00] = 0x07
 cpu.mem[0x01] = 0x30
-cpu.mem[0x02] = 0
+cpu.mem[0x02] = 0x00
 
-cpu.mem[0x30] = 5
-cpu.mem[0x31] = 2
-cpu.mem[0x32] = 0
+cpu.mem[0x30] = 0x05
+cpu.mem[0x31] = 0x02
+cpu.mem[0x32] = 0x00
 
-cpu.mem[0x33] = 1
-cpu.mem[0x34] = 1
+cpu.mem[0x33] = 0x01
+cpu.mem[0x34] = 0x01
 cpu.mem[0x35] = 0x08
 
-cpu.mem[0x36] = 1
-cpu.mem[0x37] = 0
+cpu.mem[0x36] = 0x01
+cpu.mem[0x37] = 0x00
 cpu.mem[0x38] = 0x10
 
-cpu.mem[0x39] = 1
-cpu.mem[0x3A] = 0
+cpu.mem[0x39] = 0x01
+cpu.mem[0x3A] = 0x00
 cpu.mem[0x3B] = 0x11
 
-cpu.mem[0x3C] = 1
-cpu.mem[0x3D] = 0
+cpu.mem[0x3C] = 0x01
+cpu.mem[0x3D] = 0x00
 cpu.mem[0x3E] = 0x12
 
-cpu.mem[0x3F] = 1
-cpu.mem[0x40] = 0
+cpu.mem[0x3F] = 0x01
+cpu.mem[0x40] = 0x00
 cpu.mem[0x41] = 0x13
 
-cpu.mem[0x42] = 1
-cpu.mem[0x43] = 0
+cpu.mem[0x42] = 0x01
+cpu.mem[0x43] = 0x00
 cpu.mem[0x44] = 0x14
 
-cpu.mem[0x45] = 1
-cpu.mem[0x46] = 0
+cpu.mem[0x45] = 0x01
+cpu.mem[0x46] = 0x00
 cpu.mem[0x47] = 0x15
 
-cpu.mem[0x48] = 1
-cpu.mem[0x49] = 0
+cpu.mem[0x48] = 0x01
+cpu.mem[0x49] = 0x00
 cpu.mem[0x4A] = 0x16
 
-cpu.mem[0x4B] = 1
-cpu.mem[0x4C] = 0
+cpu.mem[0x4B] = 0x01
+cpu.mem[0x4C] = 0x00
 cpu.mem[0x4D] = 0x17
 
-cpu.mem[0x4E] = 10
-cpu.mem[0x4F] = 0
-cpu.mem[0x50] = 0
+cpu.mem[0x4E] = 0x0A
+cpu.mem[0x4F] = 0x00
+cpu.mem[0x50] = 0x00
 
 cpu.run()
 
 limiar = cpu.mem[0x08]
-cont = 0
+contador = 0
 
 for i in range(0x10, 0x18):
     if cpu.mem[i] > limiar:
-        cont += 1
+        contador += 1
 
-cpu.mem[0x20] = cont
+cpu.mem[0x20] = contador
 
-print("\nResultado:", cpu.mem[0x20])
+print("\nLimiar:", cpu.mem[0x08])
+print("Valores:", cpu.mem[0x10:0x18])
+print("Resultado em 0x20:", cpu.mem[0x20])
